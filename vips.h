20583662e8fdf5f50ba85bc19c2413576ddd43a7
DarthSim <darthsim@gmail.com>
2017-09-27 23:22:36 +0600

    Better png save

--- vips.h
+++ vips.h
@@ -125,7 +125,7 @@ vips_jpegsave_go(VipsImage *in, void **buf, size_t *len, int strip, int quality,
 int
 vips_pngsave_go(VipsImage *in, void **buf, size_t *len)
 {
-  return vips_pngsave_buffer(in, buf, len, NULL);
+  return vips_pngsave_buffer(in, buf, len, "filter", VIPS_FOREIGN_PNG_FILTER_NONE, NULL);
 }
 
 int
