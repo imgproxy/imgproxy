004841fca4661a850e4e3640784b1d4dcab75bd5
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2018-02-06 10:42:26 +0000

    Generate random value on server startup to be used in ETag calculation

--- etag.go
+++ etag.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"crypto/rand"
 	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net/http"
 )
+
+var randomValue []byte
+
+// run on the server startup
+func init() {
+	if conf.ETagEnabled {
+		randomValue = make([]byte, 16)
+		rand.Read(randomValue)
+		log.Printf("ETag support is activated. Generated random value to be used for ETag calculation: %s\n",
+			fmt.Sprintf("%x", randomValue))
+	}
+}
 
 // check whether client's ETag matches current response body.
 // - if the IMGPROXY_USE_ETAG env var is unset, this function always returns false
@@ -35,7 +49,7 @@ func calculateHashSumFor(b []byte, po *processingOptions) string {
 	// SHA checksum consists of image, porcessing options and random value, generated on server startup.
 	hash.Write(b)
 	binary.Write(hash, binary.LittleEndian, *po)
-	hash.Write([]byte(conf.Salt))
+	hash.Write(randomValue)
 
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
