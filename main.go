d6f5559fa7d0d05db57eb4fb6a61ac7e8cf3731a
DarthSim <darthsim@gmail.com>
2018-10-05 22:34:57 +0600

    Disable profiling

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 
-	"net/http"
 	_ "net/http/pprof"
 )
 
@@ -14,10 +12,6 @@ const version = "2.0.0"
 type ctxKey string
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
 	s := startServer()
 
 	stop := make(chan os.Signal, 1)
