029b7191000393ce75a5cb9a9b606c1adc8bbf7c
selul <marius.cristea@vertistudio.com>
2019-02-07 14:44:55 +0200

    improve svg format detection

--- svg.go
+++ svg.go
@@ -19,4 +19,14 @@ func init() {
 			return image.Config{ColorModel: color.RGBAModel, Width: 1, Height: 1}, nil
 		},
 	)
+	image.RegisterFormat(
+		"svg",
+		"<svg",
+		func(io.Reader) (image.Image, error) {
+			return image.NewRGBA(image.Rect(0, 0, 1, 1)), nil
+		},
+		func(io.Reader) (image.Config, error) {
+			return image.Config{ColorModel: color.RGBAModel, Width: 1, Height: 1}, nil
+		},
+	)
 }
