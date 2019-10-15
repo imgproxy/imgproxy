f9db1b28adf48c413c45c3aa5722254fbbeb574a
DarthSim <darthsim@gmail.com>
2019-10-15 18:21:59 +0600

    More predictable "Edit on GitHub" behavior

--- docs/assets/docsify-edit-link.js
+++ docs/assets/docsify-edit-link.js
@@ -1,39 +1,26 @@
 ; (function (win) {
   win.EditOnGithubPlugin = {}
 
-  function create(docBase, docEditBase, title) {
-    title = title || 'Edit on github'
-    docEditBase = docEditBase || docBase.replace(/\/blob\//, '/edit/')
-
-    function editDoc(event, vm) {
-      var docName = vm.route.file
-
-      if (docName) {
-        var editLink = docEditBase + docName
-        window.open(editLink)
-        event.preventDefault()
-        return false
-      } else {
-        return true
-      }
-    }
-
-    win.EditOnGithubPlugin.editDoc = editDoc
+  function create(docBase) {
+    var docEditBase = docBase.replace(/\/blob\//, '/edit/')
 
     return function (hook, vm) {
-      win.EditOnGithubPlugin.onClick = function (event) {
-        EditOnGithubPlugin.editDoc(event, vm)
-      }
-
-      var header = [
-        '<a class="github-edit-btn" title="Edit on GitHub" href="',
-        docBase,
-        '" target="_blank" onclick="EditOnGithubPlugin.onClick(event)">',
-        'Edit on <strong>GitHub</strong>',
-        '</a>'
-      ].join('')
-
       hook.afterEach(function (html) {
+        var url = docBase
+        var docName = vm.route.file
+
+        if (docName) {
+          url = docEditBase + docName
+        }
+
+        var header = [
+          '<a class="github-edit-btn" title="Edit on GitHub" href="',
+          url,
+          '" target="_blank">',
+          'Edit on <strong>GitHub</strong>',
+          '</a>'
+        ].join('')
+
         return html + header
       })
     }
