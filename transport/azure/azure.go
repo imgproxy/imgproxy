5c386509e29f8908ced25722060025dc6f5e14f8
Alan Brault <alan.brault@hardwareandtools.com>
2022-12-12 09:34:41 -0500

    task: implement ranged request support
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>

--- transport/azure/azure.go
+++ transport/azure/azure.go
@@ -1,10 +1,16 @@
 package azure
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
+	"github.com/imgproxy/imgproxy/v3/httprange"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
@@ -14,7 +20,7 @@ import (
 )
 
 type transport struct {
-	svc *azblob.Client
+	client *azblob.Client
 }
 
 func New() (http.RoundTripper, error) {
@@ -24,6 +30,10 @@ func New() (http.RoundTripper, error) {
 		err                    error
 		sharedKeyCredential    *azblob.SharedKeyCredential
 	)
+
+	if config.ABSName == "" {
+		return nil, errors.New("IMGPROXY_ABS_NAME must be set")
+	}
 
 	endpoint := config.ABSEndpoint
 	if len(endpoint) == 0 {
@@ -57,28 +67,53 @@ func New() (http.RoundTripper, error) {
 	return transport{client}, nil
 }
 
-func (t transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
+func (t transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	container := req.URL.Host
 	key := req.URL.Path
 
-	header := make(http.Header)
+	var statusCode = http.StatusOK
 
-	result, err := t.svc.DownloadStream(req.Context(), container, strings.TrimPrefix(key, "/"), nil)
+	header := make(http.Header)
+	opts := &blob.DownloadStreamOptions{}
+
+	if r := req.Header.Get("Range"); len(r) != 0 {
+		start, end, err := httprange.Parse(r)
+		if err != nil {
+			return httprange.InvalidHTTPRangeResponse(req), err
+		}
+
+		if end != 0 {
+			length := end - start + 1
+			if end < 0 {
+				length = -1
+			}
+
+			opts.Range = blob.HTTPRange{
+				Offset: start,
+				Count:  length,
+			}
+		}
+
+		statusCode = http.StatusPartialContent
+	}
+
+	result, err := t.client.DownloadStream(req.Context(), container, strings.TrimPrefix(key, "/"), opts)
 	if err != nil {
 		if azError, ok := err.(*azcore.ResponseError); !ok || azError.StatusCode < 100 || azError.StatusCode == 301 {
 			return nil, err
 		} else {
+			body := strings.NewReader(azError.Error())
 			return &http.Response{
 				StatusCode:    azError.StatusCode,
 				Proto:         "HTTP/1.0",
 				ProtoMajor:    1,
 				ProtoMinor:    0,
 				Header:        header,
-				ContentLength: *result.ContentLength,
-				Body:          ctxreader.New(req.Context(), result.Body, true),
-				Close:         true,
+				ContentLength: int64(body.Len()),
+				Body:          io.NopCloser(body),
+				Close:         false,
 				Request:       req,
-			}, nil
+			}, err
 		}
 	}
 
@@ -97,12 +132,24 @@ func (t transport) RoundTrip(req *http.Request) (resp *http.Response, err error)
 				Body:          nil,
 				Close:         false,
 				Request:       req,
-			}, nil
+			}, err
 		}
 	}
 
+	header.Set("Accept-Ranges", "bytes")
+	header.Set("Content-Length", strconv.FormatInt(*result.ContentLength, 10))
+	header.Set("Content-Type", *result.ContentType)
+
+	if result.ContentRange != nil {
+		header.Set("Content-Range", *result.ContentRange)
+	}
+
+	if result.CacheControl != nil {
+		header.Set("Cache-Control", *result.CacheControl)
+	}
+
 	return &http.Response{
-		StatusCode:    http.StatusOK,
+		StatusCode:    statusCode,
 		Proto:         "HTTP/1.0",
 		ProtoMajor:    1,
 		ProtoMinor:    0,
@@ -111,5 +158,5 @@ func (t transport) RoundTrip(req *http.Request) (resp *http.Response, err error)
 		Body:          ctxreader.New(req.Context(), result.Body, true),
 		Close:         true,
 		Request:       req,
-	}, nil
+	}, err
 }
