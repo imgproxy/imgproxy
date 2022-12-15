0b9577eb7904812e3c7910f8c690a5f6aced3d2b
Alan Brault <alan.brault@visus.io>
2022-12-15 07:51:07 -0500

    Implement Azure Identity Authentication for Azure Transport (#1052)
    * refactor: implement support for azure managed identities and service principals
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * task: implement ranged request support
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: check result.ETag for nil
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: implement null guards for content-length and content-type
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: use blockblob.CountToEnd instead of -1 for length
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * Replace OTel AWS ECS detector with fixed fork
    
    * Update semconv in OTel
    
    * refactor: implement support for azure managed identities and service principals
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * task: implement ranged request support
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: check result.ETag for nil
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: implement null guards for content-length and content-type
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: use blockblob.CountToEnd instead of -1 for length
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * chore: run go mod tidy
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    * refactor: discard err returns
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    
    Signed-off-by: Alan Brault <alan.brault@hardwareandtools.com>
    Co-authored-by: DarthSim <darthsim@gmail.com>

--- transport/azure/azure.go
+++ transport/azure/azure.go
@@ -1,27 +1,40 @@
 package azure
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 
-	"github.com/Azure/azure-storage-blob-go/azblob"
-	"github.com/imgproxy/imgproxy/v3/config"
 	"github.com/imgproxy/imgproxy/v3/httprange"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blockblob"
+	"github.com/imgproxy/imgproxy/v3/config"
+	"github.com/imgproxy/imgproxy/v3/ctxreader"
 )
 
 type transport struct {
-	serviceURL *azblob.ServiceURL
+	client *azblob.Client
 }
 
 func New() (http.RoundTripper, error) {
-	credential, err := azblob.NewSharedKeyCredential(config.ABSName, config.ABSKey)
-	if err != nil {
-		return nil, err
-	}
+	var (
+		client                 *azblob.Client
+		defaultAzureCredential *azidentity.DefaultAzureCredential
+		err                    error
+		sharedKeyCredential    *azblob.SharedKeyCredential
+	)
 
-	pipeline := azblob.NewPipeline(credential, azblob.PipelineOptions{})
+	if config.ABSName == "" {
+		return nil, errors.New("IMGPROXY_ABS_NAME must be set")
+	}
 
 	endpoint := config.ABSEndpoint
 	if len(endpoint) == 0 {
@@ -32,44 +45,90 @@ func New() (http.RoundTripper, error) {
 		return nil, err
 	}
 
-	serviceURL := azblob.NewServiceURL(*endpointURL, pipeline)
+	if config.ABSKey != "" {
+		sharedKeyCredential, err = azblob.NewSharedKeyCredential(config.ABSName, config.ABSKey)
+		if err != nil {
+			return nil, err
+		}
 
-	return transport{&serviceURL}, nil
-}
+		client, err = azblob.NewClientWithSharedKeyCredential(endpointURL.String(), sharedKeyCredential, nil)
+	} else {
+		defaultAzureCredential, err = azidentity.NewDefaultAzureCredential(nil)
+		if err != nil {
+			return nil, err
+		}
 
-func (t transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	containerURL := t.serviceURL.NewContainerURL(strings.ToLower(req.URL.Host))
-	blobURL := containerURL.NewBlockBlobURL(strings.TrimPrefix(req.URL.Path, "/"))
-
-	start, end, err := httprange.Parse(req.Header.Get("Range"))
-	if err != nil {
-		return httprange.InvalidHTTPRangeResponse(req), nil
+		client, err = azblob.NewClient(endpointURL.String(), defaultAzureCredential, nil)
 	}
 
-	length := end - start + 1
-	if end <= 0 {
-		length = azblob.CountToEnd
-	}
-
-	get, err := blobURL.Download(req.Context(), start, length, azblob.BlobAccessConditions{}, false, azblob.ClientProvidedKeyOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	if config.ETagEnabled && start == 0 && end == azblob.CountToEnd {
-		etag := string(get.ETag())
+	return transport{client}, nil
+}
 
-		if etag == req.Header.Get("If-None-Match") {
-			if body := get.Response().Body; body != nil {
-				get.Response().Body.Close()
+func (t transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	container := req.URL.Host
+	key := req.URL.Path
+
+	var statusCode = http.StatusOK
+
+	header := make(http.Header)
+	opts := &blob.DownloadStreamOptions{}
+
+	if r := req.Header.Get("Range"); len(r) != 0 {
+		start, end, err := httprange.Parse(r)
+		if err != nil {
+			return httprange.InvalidHTTPRangeResponse(req), err
+		}
+
+		if end != 0 {
+			length := end - start + 1
+			if end <= 0 {
+				length = blockblob.CountToEnd
 			}
 
+			opts.Range = blob.HTTPRange{
+				Offset: start,
+				Count:  length,
+			}
+		}
+
+		statusCode = http.StatusPartialContent
+	}
+
+	result, err := t.client.DownloadStream(req.Context(), container, strings.TrimPrefix(key, "/"), opts)
+	if err != nil {
+		if azError, ok := err.(*azcore.ResponseError); !ok || azError.StatusCode < 100 || azError.StatusCode == 301 {
+			return nil, err
+		} else {
+			body := strings.NewReader(azError.Error())
+			return &http.Response{
+				StatusCode:    azError.StatusCode,
+				Proto:         "HTTP/1.0",
+				ProtoMajor:    1,
+				ProtoMinor:    0,
+				Header:        header,
+				ContentLength: int64(body.Len()),
+				Body:          io.NopCloser(body),
+				Close:         false,
+				Request:       req,
+			}, nil
+		}
+	}
+
+	if config.ETagEnabled && result.ETag != nil {
+		azETag := string(*result.ETag)
+		header.Set("ETag", azETag)
+
+		if etag := req.Header.Get("If-None-Match"); len(etag) > 0 && azETag == etag {
 			return &http.Response{
 				StatusCode:    http.StatusNotModified,
 				Proto:         "HTTP/1.0",
 				ProtoMajor:    1,
 				ProtoMinor:    0,
-				Header:        make(http.Header),
+				Header:        header,
 				ContentLength: 0,
 				Body:          nil,
 				Close:         false,
@@ -78,5 +137,33 @@ func (t transport) RoundTrip(req *http.Request) (resp *http.Response, err error)
 		}
 	}
 
-	return get.Response(), nil
+	header.Set("Accept-Ranges", "bytes")
+
+	if result.ContentLength != nil {
+		header.Set("Content-Length", strconv.FormatInt(*result.ContentLength, 10))
+	}
+
+	if result.ContentType != nil {
+		header.Set("Content-Type", *result.ContentType)
+	}
+
+	if result.ContentRange != nil {
+		header.Set("Content-Range", *result.ContentRange)
+	}
+
+	if result.CacheControl != nil {
+		header.Set("Cache-Control", *result.CacheControl)
+	}
+
+	return &http.Response{
+		StatusCode:    statusCode,
+		Proto:         "HTTP/1.0",
+		ProtoMajor:    1,
+		ProtoMinor:    0,
+		Header:        header,
+		ContentLength: *result.ContentLength,
+		Body:          ctxreader.New(req.Context(), result.Body, true),
+		Close:         true,
+		Request:       req,
+	}, nil
 }
