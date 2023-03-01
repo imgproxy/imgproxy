564865f797e0b85dcee717c87d03a41e4921a56e
Ondřej Misák <email@ondrejmisak.cz>
2023-03-01 14:57:47 +0100

    Update example of signature for javascript
    The approach used in the demonstration is relatively complex and sophisticated. It can be done in a simpler, typed, more modern and safer way.

--- examples/signature.js
+++ examples/signature.js
@@ -1,23 +1,15 @@
-const createHmac = require('create-hmac')
+import { getImageUrl } from '@misaon/imgproxy'
 
 const KEY = '943b421c9eb07c830af81030552c86009268de4e532ba2ee2eab8247c6da0881'
 const SALT = '520f986b998545b4785e0defbc4f3c1203f22de2374a3d53cb7a7fe9fea309c5'
 
-const urlSafeBase64 = (string) => {
-  return Buffer.from(string).toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
-}
+const imageUrl = getImageUrl('https://img.example.com/pretty/image.jpg', {
+    secret: KEY,
+    salt: SALT,
+    modifiers: {
+        width: 300,
+        height: 250,
+    }
+})
 
-const hexDecode = (hex) => Buffer.from(hex, 'hex')
-
-const sign = (salt, target, secret) => {
-  const hmac = createHmac('sha256', hexDecode(secret))
-  hmac.update(hexDecode(salt))
-  hmac.update(target)
-  return urlSafeBase64(hmac.digest())
-}
-
-const path = "/rs:fit:300:300/plain/http://img.example.com/pretty/image.jpg"
-
-const signature = sign(SALT, path, KEY)
-const result = `/${signature}${path}`
-console.log(result)
+console.log(imageUrl)
