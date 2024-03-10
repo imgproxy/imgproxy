a31e4e7c7980eeacd3c60f34ccb42215eab412cc
DarthSim <darthsim@gmail.com>
2024-03-10 21:03:53 +0300

    Add Bugsnag logger

--- errorreport/bugsnag/bugsnag.go
+++ errorreport/bugsnag/bugsnag.go
@@ -4,11 +4,18 @@ import (
 	"net/http"
 
 	"github.com/bugsnag/bugsnag-go/v2"
+	"github.com/sirupsen/logrus"
 
 	"github.com/imgproxy/imgproxy/v3/config"
 )
 
 var enabled bool
+
+type logger struct{}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	logrus.WithField("source", "bugsnag").Debugf(format, v...)
+}
 
 func Init() {
 	if len(config.BugsnagKey) > 0 {
@@ -16,6 +23,7 @@ func Init() {
 			APIKey:       config.BugsnagKey,
 			ReleaseStage: config.BugsnagStage,
 			PanicHandler: func() {}, // Disable forking the process
+			Logger:       logger{},
 		})
 		enabled = true
 	}
