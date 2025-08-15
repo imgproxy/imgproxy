2191ee70b1b722f63eea9961ae1601cd18635ea6
DarthSim <darthsim@gmail.com>
2025-08-15 19:45:37 +0300

    Return detector errors from imagetype.Detect

--- imagetype/errors.go
+++ imagetype/errors.go
@@ -7,8 +7,25 @@ import (
 )
 
 type (
+	TypeDetectionError struct{ error }
 	UnknownFormatError struct{}
 )
+
+func newTypeDetectionError(err error) error {
+	return ierrors.Wrap(
+		TypeDetectionError{err},
+		1,
+		ierrors.WithStatusCode(http.StatusUnprocessableEntity),
+		ierrors.WithPublicMessage("Failed to detect source image type"),
+		ierrors.WithShouldReport(false),
+	)
+}
+
+func (e TypeDetectionError) Error() string {
+	return "Failed to detect image type: " + e.error.Error()
+}
+
+func (e TypeDetectionError) Unwrap() error { return e.error }
 
 func newUnknownFormatError() error {
 	return ierrors.Wrap(
