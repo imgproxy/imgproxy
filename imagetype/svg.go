b53a88f402b992ec489e9f176816be84be30ac4e
DarthSim <darthsim@gmail.com>
2025-08-15 19:46:23 +0300

    Return error from SVG detector unless it is EOF or parser.Error

--- imagetype/svg.go
+++ imagetype/svg.go
@@ -1,6 +1,8 @@
 package imagetype
 
 import (
+	"errors"
+	"io"
 	"strings"
 
 	"github.com/imgproxy/imgproxy/v3/bufreader"
@@ -22,7 +24,20 @@ func IsSVG(r bufreader.ReadPeeker) (Type, error) {
 
 		switch tt {
 		case xml.ErrorToken:
-			return Unknown, nil
+			err := l.Err()
+
+			if err == io.EOF || err == io.ErrUnexpectedEOF {
+				// EOF or unexpected EOF means we don't have enough data to determine the type
+				return Unknown, nil
+			}
+
+			var perr *parse.Error
+			if errors.As(err, &perr) {
+				// If the error is a parse error, we can assume that the data is not SVG
+				return Unknown, nil
+			}
+
+			return Unknown, err
 
 		case xml.StartTagToken:
 			tag := strings.ToLower(string(l.Text()))
