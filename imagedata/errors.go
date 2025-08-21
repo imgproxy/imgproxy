3f205451c7346bec364b6ef598195a817fb77539
DarthSim <darthsim@gmail.com>
2025-08-21 18:30:18 +0300

    Use imagefetcher.WrapError in wrapDownloadError

--- imagedata/errors.go
+++ imagedata/errors.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/imgproxy/imgproxy/v3/ierrors"
+	"github.com/imgproxy/imgproxy/v3/imagefetcher"
 )
 
 func wrapDownloadError(err error, desc string) error {
 	return ierrors.Wrap(
-		err, 0,
+		imagefetcher.WrapError(err), 0,
 		ierrors.WithPrefix(fmt.Sprintf("can't download %s", desc)),
 	)
 }
