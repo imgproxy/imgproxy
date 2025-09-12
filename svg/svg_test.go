0625054342405534bb20d4f5b39002c86a2b8481
DarthSim <darthsim@gmail.com>
2025-09-12 13:54:22 +0300

    Simplify svg/svg-test.go

--- svg/svg_test.go
+++ svg/svg_test.go
@@ -1,53 +1,54 @@
 package svg
 
 import (
-	"os"
-	"path/filepath"
+	"bytes"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 
-	"github.com/imgproxy/imgproxy/v3/config"
-	"github.com/imgproxy/imgproxy/v3/fetcher"
 	"github.com/imgproxy/imgproxy/v3/imagedata"
+	"github.com/imgproxy/imgproxy/v3/imagetype"
 	"github.com/imgproxy/imgproxy/v3/testutil"
 )
 
 type SvgTestSuite struct {
-	idf *imagedata.Factory
 	suite.Suite
+
+	testData *testutil.TestDataProvider
 }
 
 func (s *SvgTestSuite) SetupSuite() {
-	config.Reset()
-
-	c := fetcher.NewDefaultConfig()
-	f, err := fetcher.New(&c)
-	s.Require().NoError(err)
-
-	s.idf = imagedata.NewFactory(f)
+	s.testData = testutil.NewTestDataProvider(s.T)
 }
 
 func (s *SvgTestSuite) readTestFile(name string) imagedata.ImageData {
-	wd, err := os.Getwd()
+	data := s.testData.Read(name)
+	return imagedata.NewFromBytesWithFormat(imagetype.SVG, data)
+}
+
+func (s *SvgTestSuite) compare(expected, actual imagedata.ImageData) {
+	expectedData, err := io.ReadAll(expected.Reader())
 	s.Require().NoError(err)
 
-	data, err := os.ReadFile(filepath.Join(wd, "..", "testdata", name))
+	actualData, err := io.ReadAll(actual.Reader())
 	s.Require().NoError(err)
 
-	d, err := s.idf.NewFromBytes(data)
-	s.Require().NoError(err)
+	// Trim whitespace to not care about ending newlines in test files
+	expectedData = bytes.TrimSpace(expectedData)
+	actualData = bytes.TrimSpace(actualData)
 
-	return d
+	s.Require().Equal(expectedData, actualData)
 }
 
 func (s *SvgTestSuite) TestSanitize() {
 	origin := s.readTestFile("test1.svg")
 	expected := s.readTestFile("test1.sanitized.svg")
-	actual, err := Sanitize(origin)
 
+	actual, err := Sanitize(origin)
 	s.Require().NoError(err)
-	s.Require().True(testutil.ReadersEqual(s.T(), expected.Reader(), actual.Reader()))
+
+	s.compare(expected, actual)
 }
 
 func TestSvg(t *testing.T) {
