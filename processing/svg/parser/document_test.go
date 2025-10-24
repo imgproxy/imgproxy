b2f64941516802a8c2829775aa9fddb49a128b5b
DarthSim <darthsim@gmail.com>
2025-10-24 23:29:34 +0300

    Polish XML parser tests; Add XML parser benchmark

--- processing/svg/parser/document_test.go
+++ processing/svg/parser/document_test.go
@@ -9,20 +9,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const svgSampleSetURL = "https://www.w3.org/Graphics/SVG/Test/Overview.html"
-
 func TestDocumentParsing(t *testing.T) {
-	path := os.Getenv("TEST_SVG_PATH")
-	if path == "" {
-		t.Skipf("Use TEST_SVG_PATH=/path/to/svg to run svg sample test. You can get SVG test suite here: %s", svgSampleSetURL)
-	}
-
-	imagesDir, err := filepath.Abs(path)
+	testImagesPath, err := filepath.Abs("../../../testdata/test-images/svg-test-suite")
 	require.NoError(t, err)
 
-	filesTested := 0
-
-	err = filepath.Walk(imagesDir, func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(testImagesPath, func(path string, info os.FileInfo, err error) error {
 		require.NoError(t, err)
 
 		// Skip directories
@@ -34,8 +25,6 @@ func TestDocumentParsing(t *testing.T) {
 		if filepath.Ext(path) != ".svg" {
 			return nil
 		}
-
-		filesTested++
 
 		// Open the SVG file
 		file, err := os.Open(path)
@@ -62,8 +51,4 @@ func TestDocumentParsing(t *testing.T) {
 	})
 
 	require.NoError(t, err)
-
-	if filesTested == 0 {
-		t.Errorf("No SVG files were found in %s", imagesDir)
-	}
 }
