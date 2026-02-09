4fd0e5344973b1659db726e9da01e8d87ebb83c4
Viktor Sokolov <gzigzigzeo@gmail.com>
2026-02-09 15:00:17 +0100

    Removed old config ns

--- init.go
+++ init.go
@@ -7,7 +7,6 @@ import (
 
 	"go.uber.org/automaxprocs/maxprocs"
 
-	"github.com/imgproxy/imgproxy/v3/config"
 	"github.com/imgproxy/imgproxy/v3/env"
 	"github.com/imgproxy/imgproxy/v3/logger"
 	"github.com/imgproxy/imgproxy/v3/vips"
@@ -28,15 +27,6 @@ func Init(ctx context.Context) error {
 	if err := logger.Init(logCfg); err != nil {
 		return err
 	}
-
-	// NOTE: This is temporary workaround. We have to load env vars in config.go before
-	// actually configuring ImgProxy instance because for now we use it as a source of truth.
-	// Will be removed once we move env var loading to imgproxy.go
-	if err := config.Configure(); err != nil {
-		// we moved validations to specific config files, hence, no need to return err
-		slog.Warn("old config validation warning", "err", err)
-	}
-	// NOTE: End of temporary workaround.
 
 	maxprocs.Set(maxprocs.Logger(func(msg string, args ...any) {
 		slog.Debug(fmt.Sprintf(msg, args...))
