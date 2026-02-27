b1e31bd51df47dad84dcaa1efd249a9683fbca33
Viktor Sokolov <gzigzigzeo@gmail.com>
2026-02-27 18:06:41 +0100

    Additional helpers in conditional.go

--- httpheaders/conditional.go
+++ httpheaders/conditional.go
@@ -13,13 +13,19 @@ import (
 // - The second boolean indicates that the ETag matches the If-None-Match value,
 // meaning the resource has not been modified.
 func CompareEtag(reqHeader, respHeader http.Header) (bool, bool) {
-	etag := respHeader.Get(Etag)
-	if etag == "" {
+	ifNoneMatch := reqHeader.Get(IfNoneMatch)
+	if ifNoneMatch == "" {
 		return false, false
 	}
 
-	ifNoneMatch := reqHeader.Get(IfNoneMatch)
-	if ifNoneMatch == "" {
+	return CompareEtagValue(ifNoneMatch, respHeader)
+}
+
+// CompareEtagValue same method as above, but accepts the raw If-None-Match value
+// instead of the request header.
+func CompareEtagValue(ifNoneMatch string, respHeader http.Header) (bool, bool) {
+	etag := respHeader.Get(Etag)
+	if etag == "" {
 		return false, false
 	}
 
@@ -42,22 +48,27 @@ func UnquoteEtag(etag string) string {
 // - The second boolean indicates that the resource has not been modified since the time specified
 // in the If-Modified-Since header.
 func CompareLastModified(reqHeader, respHeader http.Header) (bool, bool) {
+	ifModifiedSinceStr := reqHeader.Get(IfModifiedSince)
+	if ifModifiedSinceStr == "" {
+		return false, false
+	}
+
+	return CompareLastModifiedValue(ifModifiedSinceStr, respHeader)
+}
+
+// CompareLastModifiedValue same as above, but accepts the raw If-Modified-Since value
+func CompareLastModifiedValue(ifModifiedSinceStr string, respHeader http.Header) (bool, bool) {
+	ifModifiedSince, err := http.ParseTime(ifModifiedSinceStr)
+	if err != nil {
+		return false, false
+	}
+
 	lastModifiedStr := respHeader.Get(LastModified)
 	if lastModifiedStr == "" {
 		return false, false
 	}
 
 	lastModified, err := http.ParseTime(lastModifiedStr)
-	if err != nil {
-		return false, false
-	}
-
-	ifModifiedSinceStr := reqHeader.Get(IfModifiedSince)
-	if ifModifiedSinceStr == "" {
-		return false, false
-	}
-
-	ifModifiedSince, err := http.ParseTime(ifModifiedSinceStr)
 	if err != nil {
 		return false, false
 	}
